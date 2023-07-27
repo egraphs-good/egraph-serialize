cc560e26f66001391075aff1a058142c598ca061
Saul Shanabrook <s.shanabrook@gmail.com>
2023-07-27 11:01:58 -0400

    Fix color generation

--- src/graphviz.rs
+++ src/graphviz.rs
@@ -34,12 +34,19 @@ impl EGraph {
     }
 
     fn to_graphviz(&self) -> Graph {
-        // 1. Group nodes by class and create mapping from each node ID to its class
+        // 1. Group nodes by typ and class
         let mut class_nodes = std::collections::HashMap::new();
+        //  and create mapping from each node ID to its class
         let mut node_to_class = std::collections::HashMap::new();
         for (node_id, node) in &self.nodes {
+            let typ = self
+                .class_data
+                .get(&node.eclass)
+                .and_then(|data| data.typ.clone());
             node_to_class.insert(node_id.clone(), node.eclass.clone());
             class_nodes
+                .entry(typ)
+                .or_insert_with(std::collections::HashMap::new)
                 .entry(node.eclass.clone())
                 .or_insert_with(Vec::new)
                 .push((node_id.clone(), node));
@@ -73,66 +80,58 @@ impl EGraph {
         // Mapping of sort names to color index
         let mut typ_colors = std::collections::HashMap::new();
 
-        for (class_id, nodes) in class_nodes {
-            let mut inner_stmts = vec![];
+        for (typ, class_to_node) in class_nodes {
+            let next_color = typ_colors.len() % N_COLORS + INITIAL_COLOR;
+            let color = typ_colors.entry(typ).or_insert(next_color);
+            stmts.push(stmt!(attr!("fillcolor", color)));
+            for (class_id, nodes) in class_to_node {
+                let mut inner_stmts = vec![];
 
-            // Add color based on type
-            let typ = self
-                .class_data
-                .get(&class_id)
-                .and_then(|data| data.typ.clone());
-            if let Some(typ) = typ {
-                // Lookup color index for typ, or assign a new one, wrapping around
-                let next_color = typ_colors.len() % N_COLORS + 1;
-                let color = typ_colors.entry(typ).or_insert(next_color);
-                inner_stmts.push(stmt!(attr!("fillcolor", color)));
-            }
-
-            // Add nodes
-            for (node_id, node) in nodes {
-                let label = node.op.as_ref();
-                let tooltip = format!("{}: {}", class_id, node_id);
-                let html_label = html_label(label, node.children.len());
-                let quoted_tooltip = quote(&tooltip);
-                let quoted_node_id = quote(node_id.as_ref());
-                // Add edges
-                for (i, child) in node.children.iter().enumerate() {
-                    let source = node_id!(quoted_node_id, port!(id!(i), "s"));
-                    let target = node_id!(quote(child.as_ref()));
-                    let child_eclass = node_to_class.get(child).unwrap();
-                    let child_subgraph_id = format!("cluster_{}", child_eclass);
-                    let edge =
-                        edge!(source => target; EdgeAttributes::lhead(quote(&child_subgraph_id)));
-                    // Make sure edge is part of outer statements so it doesn't add nodes to the subgraph which
-                    // don't belong there
-                    stmts.push(stmt!(edge));
+                // Add nodes
+                for (node_id, node) in nodes {
+                    let label = node.op.as_ref();
+                    let tooltip = format!("{}: {}", class_id, node_id);
+                    let html_label = html_label(label, node.children.len());
+                    let quoted_tooltip = quote(&tooltip);
+                    let quoted_node_id = quote(node_id.as_ref());
+                    // Add edges
+                    for (i, child) in node.children.iter().enumerate() {
+                        let source = node_id!(quoted_node_id, port!(id!(i), "s"));
+                        let target = node_id!(quote(child.as_ref()));
+                        let child_eclass = node_to_class.get(child).unwrap();
+                        let child_subgraph_id = format!("cluster_{}", child_eclass);
+                        let edge = edge!(source => target; EdgeAttributes::lhead(quote(&child_subgraph_id)));
+                        // Make sure edge is part of outer statements so it doesn't add nodes to the subgraph which
+                        // don't belong there
+                        stmts.push(stmt!(edge));
+                    }
+                    let node = node!(quoted_node_id;NodeAttributes::label(html_label), NodeAttributes::tooltip(quoted_tooltip));
+                    inner_stmts.push(stmt!(node));
                 }
-                let node = node!(quoted_node_id;NodeAttributes::label(html_label), NodeAttributes::tooltip(quoted_tooltip));
-                inner_stmts.push(stmt!(node));
+
+                let subgraph_id = format!("cluster_{}", class_id);
+                let outer_subgraph_id = quote(&format!("outer_{}", subgraph_id));
+                let quoted_subgraph_id = quote(&subgraph_id);
+
+                let subgraph = subgraph!(outer_subgraph_id;
+                    // Disable label for now, to reduce size
+                    // NodeAttributes::label(subgraph_html_label(&typ)),
+
+                    // Nest in empty sub-graph so that we can use rank=same
+                    // https://stackoverflow.com/a/55562026/907060
+                    subgraph!(quoted_subgraph_id; subgraph!("", inner_stmts)),
+
+                    // Make outer subgraph a cluster but make it invisible, so just used for padding
+                    // https://forum.graphviz.org/t/how-to-add-space-between-clusters/1209/3
+                    SubgraphAttributes::style(quote("invis")),
+                    attr!("cluster", "true")
+                );
+                // If this is a root e-class, make the border bold
+                if self.root_eclasses.contains(&class_id) {
+                    stmts.push(stmt!(attr!("penwidth", 2)));
+                }
+                stmts.push(stmt!(subgraph));
             }
-
-            let subgraph_id = format!("cluster_{}", class_id);
-            let outer_subgraph_id = quote(&format!("outer_{}", subgraph_id));
-            let quoted_subgraph_id = quote(&subgraph_id);
-
-            let subgraph = subgraph!(outer_subgraph_id;
-                // Disable label for now, to reduce size
-                // NodeAttributes::label(subgraph_html_label(&typ)),
-
-                // Nest in empty sub-graph so that we can use rank=same
-                // https://stackoverflow.com/a/55562026/907060
-                subgraph!(quoted_subgraph_id; subgraph!("", inner_stmts)),
-
-                // Make outer subgraph a cluster but make it invisible, so just used for padding
-                // https://forum.graphviz.org/t/how-to-add-space-between-clusters/1209/3
-                SubgraphAttributes::style(quote("invis")),
-                attr!("cluster", "true")
-            );
-            // If this is a root e-class, make the border bold
-            if self.root_eclasses.contains(&class_id) {
-                stmts.push(stmt!(attr!("penwidth", 2)));
-            }
-            stmts.push(stmt!(subgraph));
         }
         // Set margin to 0 at the end again, so that total graph margin is 0, but all the clusters
         // defined above have some margins
@@ -141,7 +140,11 @@ impl EGraph {
     }
 }
 
+// Number of colors in the graphviz color scheme
+// https://graphviz.org/doc/info/colors.html
 const N_COLORS: usize = 12;
+// Initial color to use for the first type
+const INITIAL_COLOR: usize = 2;
 
 /// Returns an html label for the node with the function name and ports for each argumetn
 fn html_label(label: &str, n_args: usize) -> String {
