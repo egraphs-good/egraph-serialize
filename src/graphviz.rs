6416c3be61a8f0bbeafb7d2f02a1535a56a08d59
Saul Shanabrook <s.shanabrook@gmail.com>
2023-10-10 11:57:29 -0400

    Make colors consistent regardless of expr ordering
    Currently, if you have two graphs which use the same types, the colors
    per type might be different, depending on which type comes first.
    
    This makes comparing similar graphs harder visually.
    
    This resolves that by using a sorted map for mapping types to values,
    so that if the same set of types are used by two graphs, they will
    have the same colors.

--- src/graphviz.rs
+++ src/graphviz.rs
@@ -77,8 +77,8 @@ impl EGraph {
         ];
         // 3. Add each e-class
 
-        // Mapping of sort names to color index
-        let mut typ_colors = std::collections::HashMap::new();
+        // Mapping of sort names to color index, use BTreeMap so that keys are sorted and colors will be consistant
+        let mut typ_colors = std::collections::BTreeMap::new();
 
         for (typ, class_to_node) in class_nodes {
             let next_color = typ_colors.len() % N_COLORS + INITIAL_COLOR;
